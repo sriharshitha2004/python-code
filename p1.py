8ece2ca21566fb58ef70063df68a6ce0ce34e162
harshitha <sriharshitha.manne@gmail.com>
2026-06-11 07:58:34 +0530

    version-2

--- p1.py
+++ p1.py
@@ -4,3 +4,23 @@ if str(a)==str(a)[::-1]:
 else:
 	print(a,"is not a palindrome")
 
+
+
+
+
+
+positive_bucket = []
+negative_bucket = []
+zero = []
+
+for num in range(-25, 26):
+    if num > 0:
+        positive_bucket.append(num)
+    elif num < 0:
+        negative_bucket.append(num)
+    else:
+        zero.append(num)
+
+print("Positive Bucket:", positive_bucket)
+print("Zero:", zero)
+print("Negative Bucket:", negative_bucket)
